08e5dffcea02a95d2d529e50ce775ca82340a708
SunnyPandit26 <sunny7254sjps@gmail.com>
2024-11-21 22:49:10 +0530

    Update page2.js

--- page2.js
+++ page2.js
@@ -4,11 +4,6 @@ const texts = {
     high: "In quantum mechanics, quantum entanglement is a phenomenon.\nQuantum states of particles cannot be described independently.\nParticles remain connected even when separated by large distances."
 };
 
-// Function to add extra spaces between words
-function addExtraSpaces(text) {
-    return text.replace(/\s+/g, ' ').split(' ').join('  ');
-}
-
 // Function to add line breaks and extra spaces
 function formatText(text) {
     // Add extra spaces between words
@@ -22,28 +17,6 @@ function formatText(text) {
 Object.keys(texts).forEach(key => {
     texts[key] = formatText(texts[key]);
 });
-
-let currentPage = 1;
-let timer;
-let timeLeft = 60;
-let typingStarted = false;
-let username = '';
-let startTime, endTime;
-let currentText = ' ';
-let currentDifficulty = '';
-
-function navigateToPage(pageNumber) {
-    window.location.href = `page${pageNumber}.html`;
-}
-
-function saveToLocalStorage(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
-}
-
-function getFromLocalStorage(key) {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
-}
 
 function setDifficulty(level) {
     currentText = texts[level];
