d864ff206c3aa37628627e0e1d290832ef89952e
SunnyPandit26 <sunny7254sjps@gmail.com>
2024-11-21 21:56:03 +0530

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,7 +1,7 @@
 const texts = {
-    beginner: "The quick brown fox jumps over the lazy dog. Simple words make up this beginner test. Take your time and focus on accuracy.",
-    medium: "Programming is the process of creating a set of instructions that tell a computer how to perform a task. Programming can be done using many programming languages.",
-    high: "In quantum mechanics, quantum entanglement is a phenomenon in which the quantum states of particles cannot be described independently, even when separated by a large distance."
+    beginner: "The quick brown fox jumps over the lazy dog.\n Simple words make up this beginner test.\n Take your time and focus on accuracy.",
+    medium: "Programming is the process of creating a set of instructions.\n Programming tells a computer how to perform a task.\n It can be done using many programming languages.",
+    high: "In quantum mechanics, quantum entanglement is a phenomenon.\n Quantum states of particles cannot be described independently.\n Particles remain connected even when separated by large distances."
 };
 
 // Function to add extra spaces between words
@@ -9,9 +9,18 @@ function addExtraSpaces(text) {
     return text.replace(/\s+/g, ' ').split(' ').join('  ');
 }
 
-// Update the texts with extra spaces
+// Function to add line breaks and extra spaces
+function formatText(text) {
+    // Add extra spaces between words
+    let spacedText = text.split(' ').join('  ');
+    
+    // Ensure line breaks are preserved
+    return spacedText.replace(/\n/g, '\n\n');
+}
+
+// Update the texts with formatting
 Object.keys(texts).forEach(key => {
-    texts[key] = addExtraSpaces(texts[key]);
+    texts[key] = formatText(texts[key]);
 });
 
 let currentPage = 1;
