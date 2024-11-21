dcfc1567bc405560e8420277ba0bbcc288fe940b
SunnyPandit26 <sunny7254sjps@gmail.com>
2024-11-21 21:52:57 +0530

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,9 +1,18 @@
-// shared.js
 const texts = {
     beginner: "The quick brown fox jumps over the lazy dog. Simple words make up this beginner test. Take your time and focus on accuracy.",
     medium: "Programming is the process of creating a set of instructions that tell a computer how to perform a task. Programming can be done using many programming languages.",
     high: "In quantum mechanics, quantum entanglement is a phenomenon in which the quantum states of particles cannot be described independently, even when separated by a large distance."
 };
+
+// Function to add extra spaces between words
+function addExtraSpaces(text) {
+    return text.replace(/\s+/g, ' ').split(' ').join('  ');
+}
+
+// Update the texts with extra spaces
+Object.keys(texts).forEach(key => {
+    texts[key] = addExtraSpaces(texts[key]);
+});
 
 let currentPage = 1;
 let timer;
