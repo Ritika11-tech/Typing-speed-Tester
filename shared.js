4883f05ac4bbbead6d76c5ddc9522d34789cdece
SunnyPandit26 <sunny7254sjps@gmail.com>
2024-11-21 22:37:07 +0530

    Update shared.js

--- shared.js
+++ shared.js
@@ -3,13 +3,33 @@ const texts = {
     medium: "Programming is the process of creating a set of instructions.\n Programming tells a computer how to perform a task.\n It can be done using many programming languages.",
     high: "In quantum mechanics, quantum entanglement is a phenomenon.\n Quantum states of particles cannot be described independently.\n Particles remain connected even when separated by large distances."
 };
+
+// Function to add extra spaces between words
+function addExtraSpaces(text) {
+    return text.replace(/\s+/g, ' ').split(' ').join('  ');
+}
+
+// Function to add line breaks and extra spaces
+function formatText(text) {
+    // Add extra spaces between words
+    let spacedText = text.split(' ').join('  ');
+    
+    // Ensure line breaks are preserved
+    return spacedText.replace(/\n/g, '\n\n');
+}
+
+// Update the texts with formatting
+Object.keys(texts).forEach(key => {
+    texts[key] = formatText(texts[key]);
+});
+
 let currentPage = 1;
 let timer;
 let timeLeft = 60;
 let typingStarted = false;
 let username = '';
 let startTime, endTime;
-let currentText = '';
+let currentText = ' ';
 let currentDifficulty = '';
 
 function navigateToPage(pageNumber) {
@@ -24,19 +44,3 @@ function getFromLocalStorage(key) {
     const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : null;
 }
-
-// Updated function to format text with non-breaking spaces and double line breaks
-function formatText(text) {
-    // Replace single line breaks with double line breaks
-    let formattedText = text.replace(/\n/g, '\n\n');
-    
-    // Add extra spaces between words using non-breaking spaces
-    formattedText = formattedText.split(' ').join(' \u00A0');
-    
-    return formattedText;
-}
-
-// Update the texts with formatting
-Object.keys(texts).forEach(key => {
-    texts[key] = formatText(texts[key]);
-});
